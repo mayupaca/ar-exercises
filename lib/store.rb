7b5a728f2ed9e2ae319eb6cc4fb80ae1ab8f9de3
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-02-01 11:38:18 -0500

    finished all exercises

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0
+  }
+  validate :either_apparel
 
+  def either_apparel
+    if !womens_apparel && !mens_apparel
+      errors.add(:mens_apparel, "either mens or womens apparel must be true")
+    end
+  end
 end
